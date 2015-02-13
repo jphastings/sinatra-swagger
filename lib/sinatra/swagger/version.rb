d5e26c6119e5b0795332368269ccbfdd4930bae2
JP Hastings-Spital <jphastings@blinkbox.com>
2015-02-13 20:05:02 +0000

    __dir__ doesn't work with travis

--- lib/sinatra/swagger/version.rb
+++ lib/sinatra/swagger/version.rb
@@ -1,5 +1,5 @@
 module Sinatra
   module Swagger
-    VERSION = File.read(File.join(__dir__, "../../../VERSION")) rescue "0.0.0"
+    VERSION = File.read(File.join(File.dirname(__FILE__), "../../../VERSION")) rescue "0.0.0"
   end
 end
